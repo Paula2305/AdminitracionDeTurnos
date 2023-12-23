8f149c142e8626981325c83bcc7c5b746912af87
Paula2305 <polimoreno1976@gmail.com>
2023-12-23 14:37:42 -0300

    Validación de campos y muestra de alerta

--- js/app.js
+++ js/app.js
@@ -7,8 +7,37 @@ const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
 const formulario = document.querySelector('#nueva-cita');
-
 const contenedorCitas = document.querySelector('#citas');
+
+class Citas {
+    constructor(){
+        this.citas = [];
+    }
+}
+
+class UI {
+    imprimirAlerta(mensaje,tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger')
+        }else{
+            divMensaje.classList.add('alert-success')
+        }
+
+        divMensaje.textContent = mensaje;
+
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 5000);
+    }
+}
+
+const ui = new UI();
+const administrarCitas = new Citas();
 
 // Registrar eventos
 eventListeners();
@@ -19,6 +48,8 @@ function eventListeners(){
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 }
 
 // Objeto con la informacion 
@@ -36,5 +67,18 @@ const citaObj = {
 function datosCita(e){
     citaObj[e.target.name] = e.target.value;
 
-    console.log(citaObj)
+}
+
+// Valida y agrega una nueva cita 
+function nuevaCita(e){
+    e.preventDefault();
+
+    const {mascota, propietario,telefono,fecha,hora,sintomas} = citaObj;
+
+    if(mascota === '' || propietario === ''  || telefono === ''  || fecha === ''  || hora === ''  || sintomas === '' ){
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error')
+
+        return;
+    }
+
 }
